b19d1ac85d1e935ae9e1ed14dd63a187ae31ac55
Aditya Maroo <aditya.ma@cisinlabs.com>
2026-09-22 14:37:15 +0530

    Teacher module

---
+++ SchoolConnectApp/Teacher/Views/TeacherDashboard.swift
@@ -0,0 +1,18 @@
+//
+//  HomeDashboard.swift
+//  SchoolConnectApp
+//
+//  Created by Aditya Maroo on 22/09/2026.
+//
+
+import SwiftUI
+
+struct TeacherDashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherDashboard()
+}
